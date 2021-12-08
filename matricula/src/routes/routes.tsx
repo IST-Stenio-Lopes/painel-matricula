9b55c9b651f32061e30b5b112a12d51f0a3fb5a8
MysaelR <xxmysaelxx@gmail.com>
2021-12-08 06:07:32 -0300

    criando rotas

--- matricula/src/routes/routes.tsx
+++ matricula/src/routes/routes.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter, useLocation } from 'react-router-dom';
+import Login from '../components/login';
+import Home from '../components/home';
+
 
 const Routees = () => {
 
