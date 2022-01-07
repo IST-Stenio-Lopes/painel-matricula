cae8427b7dae6bae279c4a6bbfe2f1ccd855321d
MysaelR <xxmysaelxx@gmail.com>
2022-01-07 05:55:30 -0300

    modificando arquivos

--- matricula/src/components/home/index.tsx
+++ matricula/src/components/home/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
                 </div>
                 <div className="col-10 ">
                     <Navbar />
-                    <EditProfile />
+                    <UsersList />
                 </div>
 
             </div>
